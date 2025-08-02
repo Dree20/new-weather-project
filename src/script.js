d55b13bc19684df241f1f3964a1c2bbc55703b50
Dree20 <dreannaad@gmail.com>
2025-08-02 18:53:14 -0400

    Added axios in index.html and updated script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,11 @@
+function updateInfo(response) {
+  console.log(response.data.temperature.current);
+}
+let apiKey = "babdtd562ab541fbc49o01aca733b4ff";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=query&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(updateInfo);
+
 function changeCity(event) {
   event.preventDefault();
   let inputCityElement = document.querySelector("#input-city");
