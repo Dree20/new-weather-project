6487e17d757b85e642c493bc4deaeef962ead4ea
Dree20 <dreannaad@gmail.com>
2025-08-02 18:25:06 -0400

    Fixed issue in JS

--- src/script.js
+++ src/script.js
@@ -6,4 +6,4 @@ function changeCity(event) {
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("click", changeCity);
+searchForm.addEventListener("submit", changeCity);
