33c0c9e82e5e1c7f8d3b8666df678de0904ab54c
Dree20 <dreannaad@gmail.com>
2025-08-03 15:55:17 -0400

    Added an id to index.html and Updated script.js

--- src/script.js
+++ src/script.js
@@ -1,17 +1,24 @@
 function updateInfo(response) {
-  console.log(response.data.temperature.current);
+  let newTemperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#change-city");
+
+  cityElement.innerHTML = response.data.city;
+  newTemperatureElement.innerHTML = Math.round(temperature);
 }
-let apiKey = "babdtd562ab541fbc49o01aca733b4ff";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=query&key=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(updateInfo);
-
+function findCity(city) {
+  let apiKey = "babdtd562ab541fbc49o01aca733b4ff";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(updateInfo);
+}
 function changeCity(event) {
   event.preventDefault();
   let inputCityElement = document.querySelector("#input-city");
-  let newCityElement = document.querySelector("#change-city");
-  newCityElement.innerHTML = inputCityElement.value;
+
+  findCity(inputCityElement.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", changeCity);
+
+findCity("");
