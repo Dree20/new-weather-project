5e7f0eb4847f9ba3afcf210e718177b66152dc66
Dree20 <dreannaad@gmail.com>
2025-08-05 12:11:09 -0400

    Removed code from index.html and injected it into script.js

--- src/script.js
+++ src/script.js
@@ -51,7 +51,31 @@ function changeCity(event) {
   findCity(inputCityElement.value);
 }
 
+function displayForecast() {
+  let days = ["Mon", "Tue", "Wed", " Thu", "Fri"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="forecast-day">
+            <div class="forecast-date">${day} </div>
+            <div class="forecast-icon">⛅</div>
+            <div class="forecast-temperatures">
+              <div class="forecast-temperature"><strong>25º</strong></div>
+              <div class="forecast-temperature">17º</div>
+            </div>
+            </div>
+            `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", changeCity);
 
 findCity("Toronto");
+displayForecast();
